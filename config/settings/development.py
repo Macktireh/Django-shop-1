a0f7b1363e64de3610921f0c2bc67be35a2dbf4e
Macktireh <abdimack97@gmail.com>
2022-01-29 20:03:16 +0100

    Dockrisation du projet

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,20 +1,22 @@
 from .base import *
 
 
+# Database SQLite
 # DATABASES = {
 #     'default': {
-#         'ENGINE': env("ENGINE"),
-#         'NAME': env("POSTGRES_DB"),
-#         'USER': env("POSTGRES_USER"),
-#         'PASSWORD': env("POSTGRES_PASSWORD"),
-#         'HOST': env("POSTGRES_HOST"),
-#         'PORT': env("POSTGRES_PORT"),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 
+# Database PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': env("ENGINE"),
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': env("POSTGRES_HOST"),
+        'PORT': env("POSTGRES_PORT"),
     }
 }
